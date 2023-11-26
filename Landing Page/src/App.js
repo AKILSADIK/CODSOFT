dd374da7da0dbb18172e438bb1ce444e865ad51f
Akil Sadik M H <113614113+AKILSADIK@users.noreply.github.com>
2023-11-26 17:21:35 +0530

    Add files via upload

---
+++ Landing Page/src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+import Home from "./Components/Home/Home.jsx";
+import Navbar from "./Components/Navbar/Navbar.jsx";
+
+function App() {
+  return (
+    <div className="App">
+     <Navbar/>
+     <Home/>
+    </div>
+  );
+}
+
+export default App;
